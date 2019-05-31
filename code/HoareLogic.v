7e3c794a02a4006a600d64c5cc0de4ca3255f882
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2019-05-31 11:57:29 +0800

    update template for polynomial evaluation, include asymptotic bound loosening template, include attempt for hoare_while

--- code/HoareLogic.v
+++ code/HoareLogic.v
@@ -2,11 +2,9 @@ Require Import AB.Imp8.
 Require Import AB.PolyAB.
 
 Module Hoare_Logic.
-
 Import Assertion_D.
 Import Abstract_Pretty_Printing.
 Export Polynomial_Asympotitic_Bound.
-
 
 Inductive hoare_triple: Type :=
 | Build_hoare_triple (P: Assertion) (c: com) (Q: Assertion) (AB : AsymptoticBound).
@@ -14,28 +12,34 @@ Inductive hoare_triple: Type :=
 Notation "{{ P }}  c  {{ Q }} $ T " :=
   (Build_hoare_triple P c%imp Q T) (at level 90, c at next level).
 
-(* Provable, should be modified as new hoare rules are added
 Inductive provable {T: FirstOrderLogic}: hoare_triple -> Prop :=
-  | hoare_seq : forall (P Q R: Assertion) (c1 c2: com),
-      provable ({{P}} c1 {{Q}}) ->
-      provable ({{Q}} c2 {{R}}) ->
-      provable ({{P}} c1;;c2 {{R}})
-  | hoare_skip : forall P,
-      provable ({{P}} Skip {{P}})
-  | hoare_if : forall P Q (b: bexp) c1 c2,
-      provable ({{ P AND {[b]} }} c1 {{ Q }}) ->
-      provable ({{ P AND NOT {[b]} }} c2 {{ Q }}) ->
-      provable ({{ P }} If b Then c1 Else c2 EndIf {{ Q }})
-  | hoare_while : forall P (b: bexp) c,
-      provable ({{ P AND {[b]} }} c {{P}}) ->
-      provable ({{P}} While b Do c EndWhile {{ P AND NOT {[b]} }})
-  | hoare_asgn_bwd : forall P (X: var) (E: aexp),
-      provable ({{ P [ X |-> E] }} X ::= E {{ P }})
-  | hoare_consequence : forall (P P' Q Q' : Assertion) c,
-      P |-- P' ->
-      provable ({{P'}} c {{Q'}}) ->
-      Q' |-- Q ->
-      provable ({{P}} c {{Q}}).
-*)
+  | hoare_seq_bigtheta : forall (P Q R: Assertion) (c1 c2: com) (p1 p2 : poly) (n : logical_var),
+      provable ({{P}} c1 {{Q}} $ BigTheta p1 n) ->
+      provable ({{Q}} c2 {{R}} $ BigTheta p2 n) ->
+      provable ({{P}} c1;;c2 {{R}} $ BigTheta (poly_add p1 p2) n)
+  | hoare_while_bigo : forall P V (b: bexp) c (tp f p : poly) (v n : logical_var),
+      provable ({{ P AND {[b]} AND V==v }} c {{P AND V==(poly_eval_lv tp v)}} $ BigO f v) ->
+      (* solving recursive equation *)
+      provable ({{P AND V==n}} While b Do c EndWhile {{ P AND NOT {[b]} }} $ BigO p n)
+.
+
+Notation "|--  tr" := (provable tr) (at level 91, no associativity).
 
 End Hoare_Logic.
+(*
+Module Axiomantic_Semantics.
+Export Assertion_S.
+Export Polynomial_Asympotitic_Bound.
+
+Parameter hoare_triple : Assertion -> com -> Assertion -> AsymptoticBound -> Prop.
+
+Notation "{{ P }}  c  {{ Q }} $ T " :=
+  (hoare_triple P c%imp Q T) (at level 90, c at next level).
+
+Axiom hoare_seq_bigtheta : forall (P Q R : Assertion) (c1 c2 : com) (p1 p2 : poly),
+  exists (n : logical_var), {{P}} c1 {{Q}} $ BigTheta p1 n ->
+                            {{Q}} c2 {{R}} $ BigTheta p2 n ->
+                            {{P}} c1;;c2 {{Q}} $ BigTheta (poly_add p1 p2) n.
+
+End Axiomantic_Semantics.
+*)
--- code/PolyAB.v
+++ code/PolyAB.v
@@ -4,6 +4,8 @@ Require Import Coq.ZArith.ZArith.
 Open Scope Z.
 
 Module Polynomial.
+Import Assertion_D.
+Open Scope term_scope.
 
 (* TODO: Implement polynomial type *)
 Inductive poly : Type.
@@ -12,10 +14,18 @@ Inductive poly : Type.
 Fixpoint poly_eval (p : poly) : Z -> Z :=
   fun z => z.
 
+Print aexp'.
+(* TODO: Implement polynomial evaluation to aexp' *)
+Fixpoint poly_eval_lv (p : poly) : logical_var -> term :=
+  fun v => v+1.
+
+(* TODO: Implement polynomial addition *)
 Fixpoint poly_add (p1 p2 : poly) : poly := p1.
 
+(* TODO: Implement polynomial multiplication *)
 Fixpoint poly_mult (p1 p2 : poly) : poly := p1.
 
+Close Scope term_scope.
 End Polynomial.
 
 Module Polynomial_Asympotitic_Bound.
@@ -36,5 +46,14 @@ Definition ab_eval (La : Lassn) (T : AsymptoticBound) (a1 a2 N t : Z) : Prop :=
   | BigTheta p n => La n >= N ->
                     a1 * (poly_eval p (La n)) <= t <= a2 * (poly_eval p (La n))
   end.
+
+Reserved Notation "T1 '=<' T2" (at level 50, no associativity).
+
+Inductive loosen : AsymptoticBound -> AsymptoticBound -> Prop :=
+  | Theta2Omega : forall p n, BigTheta p n =< BigOmega p n
+  | Theta2O : forall p n, BigTheta p n =< BigO p n
+  (* TODO: real loosenings *)
+  
+  where "T1 '=<' T2" := (loosen T1 T2).
 
 End Polynomial_Asympotitic_Bound.
