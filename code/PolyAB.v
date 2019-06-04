743509f959174e070e758682d00e8c77a8aafc82
kelvin34501 <kelvin34501@163.com>
2019-06-04 13:13:48 +0800

    poly_eval_add_iso; poly_add_eval_comm

--- code/PolyAB.v
+++ code/PolyAB.v
@@ -16,6 +16,13 @@ Fixpoint poly_eval (p : poly) : Z -> Z :=
     | nil => 0
     | h :: t => h * (Z.pow z (Z.of_nat (length t))) + (poly_eval t z)
     end.
+    
+Fixpoint poly_eval' (p' : poly) : Z -> Z := 
+  fun z => 
+    match p' with
+    | nil => 0
+    | h :: t' => h + z * (poly_eval' t' z)
+    end.
 
 Open Scope term_scope.
 Print aexp'.
@@ -198,6 +205,129 @@ Proof.
   omega.
 Qed.
 
+Theorem poly_eval'_add_body_iso : forall (p1 p2 : poly) (z : Z),
+  poly_eval' (poly_add_body p1 p2) z = poly_eval' p1 z + poly_eval' p2 z.
+Proof.
+  intros.
+  revert p2.
+  induction p1; intros.
+  - rewrite poly_add_body_empty_l. simpl. reflexivity.
+  - induction p2.
+    + simpl. omega.
+    + simpl. specialize (IHp1 p2). rewrite IHp1. 
+      rewrite Z.mul_add_distr_l. omega.
+Qed.
+
+Lemma poly_eval'_cons : forall (p: poly) (a z : Z),
+  poly_eval' (a :: p) z = a + z * (poly_eval' p z).
+Proof.
+  simpl.
+   reflexivity.
+Qed.
+
+Lemma poly_eval'_nil : forall (z : Z),
+  poly_eval' nil z = 0.
+Proof.
+  intros.
+  simpl.
+  reflexivity.
+Qed.
+
+Lemma poly_eval'_app : forall (p1 p2 : poly) (z : Z),
+  poly_eval' (p1 ++ p2) z = poly_eval' p1 z + (z^(Z.of_nat (length p1))) * (poly_eval' p2 z).
+Proof.
+  intros. induction p1.
+  - assert (Z.of_nat (Datatypes.length (nil:poly)) = 0).
+    { simpl. reflexivity. }
+    Search (nil ++ _). rewrite app_nil_l.
+    rewrite H. Search (_^0). rewrite Z.pow_0_r.
+    pose proof poly_eval'_nil z. rewrite H0.
+    omega.
+  - Search (_ :: _ ++ _). rewrite <- app_comm_cons.
+    pose proof poly_eval'_cons (p1 ++ p2) a z.
+    rewrite H. clear H. rewrite IHp1.
+    rewrite Z.mul_add_distr_l.
+    assert (z * (z ^ Z.of_nat (Datatypes.length p1)) = z ^ Z.of_nat (Datatypes.length (a :: p1))).
+    { Search (_ ^ 1). pose proof Z.pow_1_r z. rewrite <- H at 1.
+      Search (_ ^ _ * _ ^ _). pose proof Z.pow_add_r z 1 (Z.of_nat (Datatypes.length p1)).
+      assert (0 <= 1). { omega. }
+      assert (0 <= Z.of_nat (Datatypes.length p1)). 
+      { clear IHp1 H H0 H1.
+        induction p1.
+        - simpl. omega.
+        - simpl. Search (0 <= Z.pos _).
+          apply Zle_0_pos.
+      } 
+      pose proof H0 H1 H2.
+      rewrite <- H3.
+      assert (1 + Z.of_nat (Datatypes.length p1) = Z.of_nat (Datatypes.length (a :: p1))).
+      { clear IHp1 H H0 H1 H2 H3.
+        Search (_ + Z.of_nat _). pose proof Nat2Z.inj_add 1 (length p1).
+        assert (Z.of_nat 1 = 1).
+        { simpl. omega. }
+        rewrite <- H0.
+        rewrite <- H.
+        Search (Z.of_nat _ = Z.of_nat _).
+        pose proof inj_eq (1 + (length p1)) (length (a::p1)).
+        assert ((1 + Datatypes.length p1)%nat = Datatypes.length (a :: p1)).
+        { simpl. reflexivity. }
+        omega.
+      }
+      rewrite H4.
+      omega.
+    }
+    rewrite <- H.
+    pose proof poly_eval'_cons p1 a z.
+    rewrite H0.
+    Search (_ * (_ * _)).
+    rewrite Z.mul_assoc.
+    omega.
+Qed.
+    
+Theorem poly_eval_poly_eval' : forall (p : poly) (z : Z),
+  poly_eval p z = poly_eval' (rev p) z.
+Proof.
+  intros.
+  induction p.
+  - simpl. reflexivity.
+  - simpl. pose proof poly_eval'_app (rev p) (a::nil) z.
+    rewrite H. simpl.
+    Search (_ * 0). rewrite Z.mul_0_r.
+    Search (_ + 0). rewrite Z.add_0_r.
+    Search (_ * _ = _ * _). rewrite Z.mul_comm.
+    Search (length (rev _) = length _). rewrite rev_length.
+    rewrite IHp. omega.
+Qed.
+
+Theorem poly_eval_add_iso : forall p1 p2 z,
+  poly_eval (poly_add p1 p2) z = poly_eval p1 z + poly_eval p2 z.
+Proof.
+  intros.
+  pose proof poly_eval_poly_eval' p1 z.
+  pose proof poly_eval_poly_eval' p2 z.
+  pose proof poly_eval_poly_eval' (poly_add p1 p2) z.
+  rewrite H. rewrite H0. rewrite H1.
+  unfold poly_add.
+  rewrite rev_involutive.
+  apply poly_eval'_add_body_iso.
+Qed.
+
+Lemma poly_add_zero: forall (p : poly) times n,
+  poly_eval (poly_add (repeat 0 times) p) n = poly_eval p n.
+Proof.
+  intros.
+  induction p.
+  - simpl.
+    rewrite poly_add_empty_r.
+    rewrite poly_eval_0s.
+    reflexivity.
+  - simpl.
+    rewrite poly_eval_add_iso.
+    simpl.
+    rewrite poly_eval_0s.
+    omega.
+Qed.
+
 Lemma poly_add_body_comm: forall p1 p2,
   poly_add_body p1 p2 = poly_add_body p2 p1.
 Proof.
@@ -250,11 +380,11 @@ Proof.
   - simpl. rewrite IHn. rewrite repeat_head_tail. reflexivity.
 Qed.
 
-Lemma poly_add_body_same_level_tail : forall p1 p2 a1 a2,
+(* Lemma poly_add_body_same_level_tail : forall p1 p2 a1 a2,
   length p1 = length p2 ->
   poly_add_body (p1 ++ a1 :: nil) (p2 ++ a2 :: nil) = poly_add_body p1 p2 ++ (a1 + a2) :: nil.
 Proof.
-Admitted.
+Admitted. *)
 
 Lemma poly_add_cons_split : forall a p,
   a::p = poly_add (a :: (repeat 0 (length p))) p.
@@ -269,9 +399,11 @@ Lemma poly_add_assoc : forall p1 p2 p3,
   poly_add (poly_add p1 p2) p3 = poly_add p1 (poly_add p2 p3).
 Admitted.
 
+(* this is already proved in poly_eval_0s *)
 Lemma zero_poly : forall n z, poly_eval (repeat 0 n) z = 0.
 Proof.
-Admitted.
+  apply poly_eval_0s.
+Qed.
 
 Lemma extract_single_term : forall a p z n, poly_eval (poly_add (a :: repeat 0 n) p) z = a * z ^ Z.of_nat n + poly_eval p z.
 Proof.
@@ -282,60 +414,19 @@ Proof.
     induction p; intros.
     + simpl. reflexivity.
     + simpl.
-  
+  (*
   revert a. revert n.
   induction p; intros.
   - rewrite poly_add_empty_r. simpl. rewrite zero_poly, repeat_length. reflexivity.
-  - Admitted.
+  - Admitted.*) Admitted.
 
 Lemma poly_add_eval_comm : forall p1 p2 z,
   poly_eval (poly_add p1 p2) z = poly_eval p1 z + poly_eval p2 z.
-(*Proof.
-  intro.
-  induction p1; intros.
-  - simpl. rewrite poly_add_empty_l. reflexivity.
-  - rewrite poly_add_cons_split.
-    assert (poly_add (a :: repeat 0 (Datatypes.length p1)) p1 = poly_add p1 (a :: repeat 0 (Datatypes.length p1))). rewrite poly_add_comm. auto.
-    rewrite H; clear H. rewrite poly_add_assoc, IHp1, IHp1.
-    rewrite <- Z.add_assoc. f_equal.
-    (*remember (a :: repeat 0 (Datatypes.length p1)) as p0.*)
-    induction p2.
-    + rewrite poly_add_empty_r. simpl. omega.
-    + rewrite poly_add_cons_split. *)
-(** Proof.
-  intro p1.
-  induction p1; intros.
-  - simpl. rewrite poly_add_empty_l. reflexivity.
-  - simpl. pose proof IHp1 p2 z.
-    assert (a * z ^ Z.of_nat (Datatypes.length p1) + poly_eval p1 z + poly_eval p2 z =
-    a * z ^ Z.of_nat (Datatypes.length p1) + poly_eval (poly_add p1 p2) z).
-    { omega. }
-    rewrite H0. clear H0.
-    pose proof IHp1 (a::p1) z.
-    simpl in H0.
+(* rename it for code consistency
 **)
 Proof.
-  intros. revert p2.
-  induction p1; intros.
-  - simpl. rewrite poly_add_empty_l. reflexivity.
-  - simpl.
-    rewrite <- Z.add_assoc.
-    rewrite <- IHp1.
-    rewrite poly_add_cons_split, poly_add_assoc.
-    remember (poly_add p1 p2) as p3.
-    clear Heqp3.
-    remember (Datatypes.length p1) as n.
-    clear Heqn.
-    revert a.
-    induction n; intros.
-    + simpl.
-    (*
-    pose proof poly_cons_eval_comm p1 a z.
-    pose proof poly_eval_repeat_length_p a p1 z.
-    rewrite <- H0.
-    *)
-    
-  (* TODO: FILL IN HERE *) Abort.
+  apply poly_eval_add_iso.
+Qed.
 (** [] *)
 
 End Polynomial.
